d25def45909f112073c4215f9b8c4dcd107763dd
Lucas Carrias <lucascarrias@outlook.com>
2021-04-13 11:08:47 -0300

    Revert "Feature/unit testing"

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
-import renderer from 'react-test-renderer';
 
 test('renders learn react link', () => {
-  const tree = renderer.create(<App />).toJSON();
-  expect(tree).toMatchSnapshot();
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
